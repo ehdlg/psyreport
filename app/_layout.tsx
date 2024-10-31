2be0729c3236560ce221dd731c0e234128710a37
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-31 20:20:18 +0100

    feat: add wrapper, justify between layout elemets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Slot } from 'expo-router';
+import { View } from 'react-native';
 import Header from '../components/Header';
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <Slot />
+      <View className='justify-between px-2 h-screen'>
+        <Slot />
+      </View>
     </>
   );
 }
