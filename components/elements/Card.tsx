16ae58697ce65641c1aff5e3525046ed0b285b16
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-31 13:26:16 +0100

    feat: add Card component

---
+++ components/elements/Card.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'expo-router';
+import { View } from 'react-native';
+import { PropsWithChildren } from 'react';
+
+export default function Card({
+  children,
+  href,
+  styles = '',
+}: PropsWithChildren & { href: string; styles?: string }) {
+  const DEFAULT_STYLES = 'p-6 text-center rounded-lg border border-neutral-200 drop-shadow-sm ';
+
+  return <View className={[DEFAULT_STYLES, styles].join(' ')}>{children}</View>;
+}
