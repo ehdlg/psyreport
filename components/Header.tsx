c94a60df8f2cbc17f10b2d1755d58566823a0335
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-01 10:07:24 +0100

    fix: remove text gradient due to incompatibility

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,8 @@ import { View, Text } from 'react-native';
 
 export default function Header() {
   return (
-    <View className='p-2 mb-4 border-b drop-shadow-sm border-neutral-200'>
-      <Text className='inline-block font-serif text-2xl italic text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-indigo-300'>
-        PsyReport
-      </Text>
+    <View className='px-2 py-6 mb-4 border-b border-blue-200 drop-shadow-sm'>
+      <Text className='inline-block font-serif text-2xl text-indigo-300'>PsyReport</Text>
     </View>
   );
 }
