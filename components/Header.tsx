fc4f7f40b381afdb40af8d5b72eec220a9ed7b7d
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-01 10:12:15 +0100

    feat(styles): incrase logo font size, modify border color

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native';
 
 export default function Header() {
   return (
-    <View className='px-2 py-6 mb-4 border-b border-blue-200 drop-shadow-sm'>
-      <Text className='inline-block font-serif text-2xl text-indigo-300'>PsyReport</Text>
+    <View className='px-2 py-6 mb-4 border-b drop-shadow-sm border-neutral-200'>
+      <Text className='inline-block font-serif text-4xl italic text-indigo-300'>PsyReport</Text>
     </View>
   );
 }
