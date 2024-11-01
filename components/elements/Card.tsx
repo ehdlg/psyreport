663de085b939bd3e266db26cdd13ac59b05b9b55
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-01 11:54:17 +0100

    feat: add touchable as card wrapper, implement navigation to card component

--- components/elements/Card.tsx
+++ components/elements/Card.tsx
@@ -1,5 +1,6 @@
-import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { PropsWithChildren } from 'react';
+import { router } from 'expo-router';
 
 export default function Card({
   children,
@@ -7,6 +8,13 @@ export default function Card({
   styles = '',
 }: PropsWithChildren & { href: string; styles?: string }) {
   const DEFAULT_STYLES = 'p-6 text-center rounded-lg border border-neutral-200 drop-shadow-sm ';
+  const navigateTo = () => {
+    return router.navigate(href);
+  };
 
-  return <View className={[DEFAULT_STYLES, styles].join(' ')}>{children}</View>;
+  return (
+    <TouchableOpacity onPress={navigateTo} className={[DEFAULT_STYLES, styles].join(' ')}>
+      {children}
+    </TouchableOpacity>
+  );
 }
