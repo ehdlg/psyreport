621b5f48d48526bb65c55a9063b8d68b7f6d7f5f
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-04 16:37:00 +0100

    feat(constants): add default form values, add default feeling value

--- constants.ts
+++ constants.ts
@@ -1,5 +1,21 @@
 export const CURRENT_DATETIME = new Date(Date.now());
+
 export const FEELING_LIMITS = {
   MIN: 1,
   MAX: 10,
+};
+export const DEFAULT_FEELING_VALUE = FEELING_LIMITS.MAX / 2;
+
+export const DEFAULT_REPORT_VALUES = {
+  date: CURRENT_DATETIME,
+  antecedent: '',
+  event: {
+    feeling: DEFAULT_FEELING_VALUE,
+    text: '',
+  },
+  reflections: {
+    feeling: DEFAULT_FEELING_VALUE,
+    text: '',
+  },
+  thoughts: '',
 };
