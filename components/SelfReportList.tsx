1ae183241be8575b47718bbfaf068ff9cfd7f82d
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-12 19:13:07 +0100

    feat(components): add SelfReportList component

---
+++ components/SelfReportList.tsx
@@ -0,0 +1,47 @@
+import { router } from 'expo-router';
+import useGetSelfReports from '../hooks/useGetSelfReports';
+import { Text, View } from 'react-native';
+import SelfReportCard from './SelfReportCard';
+import Button from './elements/Button';
+import EmptyFolder from './icons/EmptyFolder';
+
+const EmptyCondition = () => {
+  const handleRedirect = () => {
+    router.navigate('/new');
+  };
+
+  return (
+    <View className='gap-y-2 mx-auto mt-24'>
+      <View className='mx-auto'>
+        <EmptyFolder width={120} height={120} />
+      </View>
+      <Text className='text-2xl font-bold text-center text-neutral-700 text-pretty'>
+        Tu lista de autorregistros está vacía
+      </Text>
+      <Text className='text-lg text-center text-neutral-500'>
+        Crea un nuevo autorregistro y aparecerá aqui
+      </Text>
+      <View className='mx-auto mt-6 w-1/2'>
+        <Button type='primary' title='Crear autorregistro' onPress={handleRedirect} />
+      </View>
+    </View>
+  );
+};
+
+export default function SelfReportList() {
+  const { selfReports, error, isLoading } = useGetSelfReports();
+
+  if (isLoading) return <Text>Loading...</Text>;
+
+  if (error) return <Text>{error}</Text>;
+
+  if (selfReports.length === 0) return <EmptyCondition />;
+
+  return (
+    <View className='gap-y-8'>
+      {selfReports.map((report) => (
+        <SelfReportCard report={report} key={report.id} />
+      ))}
+    </View>
+  );
+}
