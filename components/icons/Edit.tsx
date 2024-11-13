9b1c8256dcae9f389edec298512c17b62630d1a0
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-13 08:51:18 +0100

    feat: change Edit icon

--- components/icons/Edit.tsx
+++ components/icons/Edit.tsx
@@ -4,22 +4,24 @@ import Svg, { Path } from 'react-native-svg';
 function Edit(props: any) {
   return (
     <Svg
+      className='w-6 h-6 text-gray-800 dark:text-white'
+      aria-hidden='true'
       xmlns='http://www.w3.org/2000/svg'
-      x-bind:width='size'
-      x-bind:height='size'
-      viewBox='0 0 24 24'
+      width={24}
+      height={24}
       fill='none'
-      stroke='currentColor'
-      x-bind:stroke-width='stroke'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      strokeWidth={2}
+      viewBox='0 0 24 24'
       {...props}
     >
-      <Path d='M14 3v4a1 1 0 0 0 1 1h4' />
-      <Path d='M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z' />
-      <Path d='M10 18l5 -5a1.414 1.414 0 0 0 -2 -2l-5 5v2h2z' />
+      <Path
+        stroke={props.stroke || 'currentColor'}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        d='M14.304 4.844l2.852 2.852M7 7H4a1 1 0 00-1 1v10a1 1 0 001 1h11a1 1 0 001-1v-4.5m2.409-9.91a2.017 2.017 0 010 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 012.852 0z'
+      />
     </Svg>
   );
 }
+
 export default Edit;
