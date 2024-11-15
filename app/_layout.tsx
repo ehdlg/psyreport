273001b12de7ae5666fd5c2c5a677a1eb4a6e805
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-15 06:28:26 +0100

    feat: set system color scheme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,15 @@
 import { Slot } from 'expo-router';
 import { View } from 'react-native';
+import { useColorScheme } from 'nativewind';
 import Header from '../components/Header';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RootSiblingParent } from 'react-native-root-siblings';
 
 export default function Layout() {
+  const { setColorScheme } = useColorScheme();
+
+  setColorScheme('system');
+
   return (
     <RootSiblingParent>
       <SafeAreaView edges={['top']}>
