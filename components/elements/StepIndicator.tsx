e4e81de31cf3688c0e34f910fe1e0d3d93d85d4a
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-15 10:28:31 +0100

    feat(styles): update StepIndicator `current` and `todo` border styles

--- components/elements/StepIndicator.tsx
+++ components/elements/StepIndicator.tsx
@@ -5,9 +5,9 @@ export default function StepIndicator({ status }: { status: StepIndicatorStatus 
   const BASIC_STYLES =
     'flex-1 py-1 rounded-xl border border-slate-200 dark:border-slate-600 transition ease-in duration-150';
   const styles: Record<StepIndicatorStatus, string> = {
-    current: 'bg-slate-200 border-indigo-300 dark:bg-slate-700 dark:border-indigo-300',
+    current: 'bg-slate-200 border-indigo-200 dark:bg-slate-700 dark:border-indigo-300',
     done: 'bg-indigo-300 border-transparent',
-    todo: 'bg-slate-100/30 dark:bg-slate-600/30 border-transparent',
+    todo: 'bg-slate-100 dark:bg-slate-600/30 dark:border-transparent border-slate-200',
   };
 
   return <View className={`${BASIC_STYLES} ${styles[status]}`}></View>;
