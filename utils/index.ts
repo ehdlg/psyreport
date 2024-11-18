53cb6f582a5677391312a034e4cad3efefa0c5dd
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-18 06:37:52 +0100

    feat(utils): add function to get the first incomplete step in multi-step form

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,6 @@
+import { INITAL_FORM_STEP, FORM_STEP } from '../constants';
+import { SelfReport } from '../types';
+
 export function formatDateWithTime(date: Date) {
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -7,4 +10,17 @@ export function formatDateWithTime(date: Date) {
   const minutes = String(date.getMinutes()).padStart(2, '0');
 
   return `${day}/${month}/${year}, ${hours}:${minutes}`;
+}
+
+export function getEditStep(report: SelfReport) {
+  for (const [key, value] of Object.entries(report)) {
+    if (
+      (typeof value === 'string' && value.length === 0) ||
+      (typeof value === 'object' && null != value && null != value.text && value.text.length === 0)
+    ) {
+      return FORM_STEP[key as keyof SelfReport] || INITAL_FORM_STEP;
+    }
+  }
+
+  return INITAL_FORM_STEP;
 }
