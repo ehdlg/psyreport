517d941c74b4c2b56f93543b610dc12608d78c42
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-11-26 15:54:15 +0100

    feat(hooks): add useAudio hook

---
+++ hooks/useAudio.ts
@@ -0,0 +1,44 @@
+import { Audio } from 'expo-av';
+import { useCallback, useEffect, useState } from 'react';
+
+export default function useAudio(uri: string | null) {
+  const [audio, setAudio] = useState<Audio.Sound | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const getAudio = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      if (null == uri) {
+        setAudio(null);
+
+        return;
+      }
+
+      const { sound } = await Audio.Sound.createAsync({ uri });
+
+      setAudio(sound);
+    } catch (_error) {
+      setError('No se encontró el audio correspondiente');
+      setAudio(null);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [uri]);
+
+  useEffect(() => {
+    getAudio();
+  }, [uri, getAudio]);
+
+  const play = async () => {
+    if (null == audio) return;
+
+    await audio.playAsync();
+  };
+
+  // TODO remove audio from file system
+  const clearAudio = () => setAudio(null);
+
+  return { play, error, isLoading, clearAudio, audioReady: null != audio && !isLoading };
+}
